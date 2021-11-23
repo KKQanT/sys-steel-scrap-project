108022ab865e61ce3966a26c0425529d55d6d9aa
peerakarn <peerakarn.j@siametrics.com>
2021-11-23 14:33:03 +0700

    add others model-bat

---
+++ src/deep_learning/domestic_baseline_gru_avg_train.bat
@@ -0,0 +1,2 @@
+cmd /k "cd ../../venv/Scripts & activate & cd ../../src/deep_learning & python domestic_baseline_gru_avg_prep.py & python domestic_baseline_gru_avg_train.py & exit"
+exit
---
+++ src/deep_learning/domestic_transformerv1_avgsel_train.bat
@@ -0,0 +1,2 @@
+cmd /k "cd ../../venv/Scripts & activate & cd ../../src/deep_learning & python domestic_transformerv1_avgsel_prep.py & python domestic_transformerv1_avgsel_train.py & exit"
+exit
