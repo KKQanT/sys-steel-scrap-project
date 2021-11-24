424257bf7a5341da9ec3117ed3bd206043a21556
peerakarn <peerakarn.j@siametrics.com>
2021-11-24 11:05:35 +0700

    def check_files

--- inference_gui.py
+++ inference_gui.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QMainWindow, QVBoxLayout, QWidget, QCheckBox, QPushButton, QMainWindow, QGridLayout
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QMainWindow, QVBoxLayout, QWidget, QCheckBox, QPushButton, QMainWindow, QGridLayout
+from os.path import exists
+import pandas as pd
+import datetime as dt
 
 import subprocess
 
@@ -50,14 +53,19 @@ class Inference(QWidget):
         chckbox_layout.addWidget(self.inference_dl_seq2seq)
         chckbox_layout.addWidget(self.merge_output)
 
-        button_layout = QVBoxLayout()
+        button_layout = QHBoxLayout()
         button_layout.addWidget(self.selectAllButton)
         button_layout.addWidget(self.clearButton)
         button_layout.addWidget(self.okButton)
 
+        self.status = QLabel("status")
+        status_layout = QHBoxLayout()
+        status_layout.addWidget(self.status)
+
         layout = QGridLayout()
         layout.addLayout(chckbox_layout, 0, 0)
-        layout.addLayout(button_layout, 0, 1)
+        layout.addLayout(status_layout, 0, 1)
+        layout.addLayout(button_layout, 1, 0)
 
         self.setLayout(layout)
 
@@ -81,22 +89,110 @@ class Inference(QWidget):
 
     def onClickOkButton(self):
         if self.update_stooq_chkbox.isChecked():
-            subprocess.call('update_steel_lme.bat')
-        if self.update_yahoo_chkbox.isChecked():
-            subprocess.call('yahoo_downloading.bat')
-        if self.inference_ml.isChecked() or self.inference_dl_3month.isChecked() or self.inference_dl_1week.isChecked() or self.inference_dl_seq2seq.isChecked():
-            subprocess.call('preprocessing.bat')
-        if self.inference_ml.isChecked():
-            subprocess.call('ml_inference.bat')
-        if self.inference_dl_3month.isChecked():
-            subprocess.call('inference_dl_3month.bat')
-        if self.inference_dl_1week.isChecked():
-            subprocess.call('inference_dl_1week.bat')
-        if self.inference_dl_seq2seq.isChecked():
-            subprocess.call('inference_dl_seq2seq.bat')
-        if self.merge_output.isChecked():
-            subprocess.call('merge_output.bat')
+            self.status.setText('updating steel lme . . .')
+            process = subprocess.Popen(['update_steel_lme.bat'])
+            process.communicate()
+            self.status.setText('updating steel lme . . . done')
 
+        if self.update_yahoo_chkbox.isChecked():
+            self.status.setText('downloading yahoo stock price . . .')
+            process = subprocess.Popen(['yahoo_downloading.bat'])
+            process.communicate()
+
+            for i in range(5):
+
+                self.status.setText('checking yahoos file . . .')
+                data_detail, yahoo_file_completed = check_files()
+                if yahoo_file_completed == True:
+                    break
+                else:
+                    self.status.setText(f'downloading yahoo stock price . . . {i}/5')
+                    process = subprocess.Popen(['yahoo_downloading.bat'])
+                    process.communicate()
+
+            self.status.setText('downloading yahoo stock price . . . done')
+
+        if self.inference_ml.isChecked() or self.inference_dl_3month.isChecked() or self.inference_dl_1week.isChecked() or self.inference_dl_seq2seq.isChecked():
+            self.status.setText('preprocessing target data . . .')
+            process = subprocess.Popen(['preprocessing.bat'])
+            process.communicate()
+            self.status.setText('preprocessing target data . . .done')
+
+        if self.inference_ml.isChecked():
+            self.status.setText('machine learning model is forecasting scrap price at the next 3months . . .')
+            process = subprocess.Popen(['ml_inference.bat'])
+            process.communicate()
+            self.status.setText('machine learning forecasting scrap price at the next 3months . . . done')
+
+        if self.inference_dl_3month.isChecked():
+            self.status.setText('deep learning model is forecasting scrap price at the next 3months . . .')
+            process = subprocess.Popen(['inference_dl_3month.bat'])
+            process.communicate()
+            self.status.setText('deep learning model is forecasting scrap price at the next 3months . . . done')
+
+        if self.inference_dl_1week.isChecked():
+            self.status.setText('deep learning model is forecasting domestic scrap price at the next week . . .')
+            process = subprocess.Popen(['inference_dl_1week.bat'])
+            process.communicate()
+            self.status.setText('deep learning model is forecasting domestic scrap price at the next week . . . Done')
+
+        if self.inference_dl_seq2seq.isChecked():
+            self.status.setText('deep learning model is forecasting domestic scrap price at 1week to 4/12 weeks . . .')
+            process = subprocess.Popen(['inference_dl_seq2seq.bat'])
+            process.communicate()
+            self.status.setText('deep learning model is forecasting domestic scrap price at 1week to 4/12 weeks . . . Done')
+
+        if self.merge_output.isChecked():
+            self.status.setText('preprocessing data for tableau . . .')
+            process = subprocess.Popen(['merge_output.bat'])
+            process.communicate()
+            self.status.setText('preprocessing data for tableau . . . done')
+
+def check_files():
+
+    data_detail = {}
+
+    try:
+        df = pd.read_csv('data/stooq/SteelScrapLME.csv')
+        df['Date'] = pd.to_datetime(df['Date'])
+        min_date = df['Date'].min()
+        max_date = df['Date'].max()
+        data_detail['Steel_scrap_lme'] = (dt.datetime.strftime(min_date, "%d/%m/%Y"), 
+                                    dt.datetime.strftime(max_date, "%d/%m/%Y"))
+    
+    except FileNotFoundError:
+        data_detail['Steel_scrap_lme'] = 'not found'
+
+    yahoo_file_completed = True
+    yahoos_stocs_to_check = [
+        '^TWII',
+        '000333.SZ',
+        '000932.SZ',
+        '600019.SS',
+        '601899.SS',
+        'AH.BK',
+        'MT',
+        'SCHN',
+        'TSM',
+        'X'
+    ]
+
+    for file_name in yahoos_stocs_to_check:
+        try:
+            df = pd.read_csv(f'data/yahoo/{file_name}.csv')
+            df['Date'] = pd.to_datetime(df['Date'])
+            min_date = df['Date'].min()
+            max_date = df['Date'].max()
+            data_detail[file_name] = (dt.datetime.strftime(min_date, "%d/%m/%Y"), 
+                                    dt.datetime.strftime(max_date, "%d/%m/%Y"))
+            if len(df) < 1000:
+                yahoo_file_completed = False
+        except FileNotFoundError:
+            data_detail[file_name] = 'not found'
+            yahoo_file_completed = False
+    
+    return data_detail, yahoo_file_completed
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
