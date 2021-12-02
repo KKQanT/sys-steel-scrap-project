fe0491de10c1362a5ca698a9d2e99ad97879f631
peerakarn <peerakarn.j@siametrics.com>
2021-12-02 14:43:06 +0700

    plot = False

--- src/deep_learning_1week/domestic_transformerv1_avgsel_inference_1week.py
+++ src/deep_learning_1week/domestic_transformerv1_avgsel_inference_1week.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     BASE_FEATURES = ['adjusted_avg_selected_manualy']
     #WINDOW = 168
     SAVE_PREDICTION_PATH = '../../output/'
-    PLOT = True
+    PLOT = False
 
     config = ConfigParser()
     config.read('infer_model_config.ini')
